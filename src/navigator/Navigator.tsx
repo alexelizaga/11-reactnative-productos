102065ce7a16f51c47f4da92881ddc4dd19bf0d1
alexelizaga <alexelizaga@icloud.com>
2022-04-28 11:37:14 +0200

    329

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {AuthContext} from '../context/AuthContext';
 import {LoginScreen} from '../screens/LoginScreen';
 import {RegisterScreen} from '../screens/RegisterScreen';
 import {ProtectedScreen} from '../screens/ProtectedScreen';
@@ -8,6 +9,7 @@ import {ProtectedScreen} from '../screens/ProtectedScreen';
 const Stack = createStackNavigator();
 
 export const Navigator = () => {
+  const {status} = useContext(AuthContext);
   return (
     <Stack.Navigator
       screenOptions={{
@@ -16,9 +18,14 @@ export const Navigator = () => {
           backgroundColor: 'white',
         },
       }}>
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-      <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+      {status !== 'authenticated' ? (
+        <>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+      )}
     </Stack.Navigator>
   );
 };
