2ae04eb3555bcbeda949dd30c2758e93b9809f16
alexelizaga <alexelizaga@icloud.com>
2022-04-28 12:20:28 +0200

    331

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, {createContext, useReducer} from 'react';
+import React, {createContext, useReducer, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import cafeApi from '../api/cafeApi';
 import {LoginData, LoginResponse, Usuario} from '../interfaces/appInterfaces';
@@ -27,6 +28,17 @@ export const AuthContext = createContext({} as AuthContextProps);
 export const AuthProvider = ({children}: any) => {
   const [state, dispatch] = useReducer(authReducer, authInitialState);
 
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const checkToken = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (!token) {
+      return dispatch({type: 'notAuthenticated'});
+    }
+  };
+
   const signIn = async ({correo, password}: LoginData) => {
     try {
       const {data} = await cafeApi.post<LoginResponse>('/auth/login', {
@@ -37,6 +49,7 @@ export const AuthProvider = ({children}: any) => {
         type: 'signUp',
         payload: {token: data.token, user: data.usuario},
       });
+      await AsyncStorage.setItem('token', data.token);
     } catch (error: any) {
       dispatch({
         type: 'addError',
